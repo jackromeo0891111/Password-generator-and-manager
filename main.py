beec3b32a446377c83d513c63f92f44877afad3e
Yeh <Jack.Yeh@arris.com>
2021-08-16 17:00:43 -0700

    modify

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=250, height=250)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(200, 200, image=lock_img)
+canvas.grid(column=1, row=1)
+
+window.mainloop()
