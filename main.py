1c716fe1d5dbc85a5e71f02cf41704f014251e42
jyeh <jackhyyeh@gmail.com>
2021-08-17 16:17:59 -0700

    commit

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open("data.txt", "a") as f:
+        f.write(f"{website} | {email} | {password}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,14 +28,16 @@ email_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
 
 website_entry = Entry(width=40)
+website_entry.focus()
 email_entry = Entry(width=40)
+email_entry.insert(0, "jack.yeh@gmail.com")
 password_entry = Entry(width=22)
 website_entry.grid(column=1, row=1, columnspan=2)
 email_entry.grid(column=1, row=2, columnspan=2)
 password_entry.grid(column=1, row=3)
 
 generate_password_button = Button(text="Generate Password", width=15)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 generate_password_button.grid(column=2, row=3)
 add_button.grid(column=1, row=4, columnspan=2)
 
