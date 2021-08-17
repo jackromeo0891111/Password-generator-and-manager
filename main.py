5f90ec40dfce24f48c3bd882225ed8342f5f5ed8
Yeh <Jack.Yeh@arris.com>
2021-08-16 17:09:18 -0700

    add-ons

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-canvas = Canvas(width=250, height=250)
+canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
-canvas.create_image(200, 200, image=lock_img)
-canvas.grid(column=1, row=1)
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=0, row=0)
 
 window.mainloop()
