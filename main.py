89da0eef2bc500a03497b713215ae9b4b5e996d1
Yeh <Jack.Yeh@arris.com>
2021-08-16 17:40:12 -0700

    modify

--- main.py
+++ main.py
@@ -12,6 +12,27 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
-canvas.grid(column=0, row=0)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+email_label = Label(text="Email/Username:")
+password_label = Label(text="Password:")
+website_label.grid(column=0, row=1)
+email_label.grid(column=0, row=2)
+password_label.grid(column=0, row=3)
+
+website_entry = Entry(width=35)
+email_entry = Entry(width=35)
+password_entry = Entry(width=21)
+website_entry.grid(column=1, row=1, columnspan=2)
+email_entry.grid(column=1, row=2, columnspan=2)
+password_entry.grid(column=1, row=3, columnspan=2)
+
+
+
+
+
+
+
 
 window.mainloop()
